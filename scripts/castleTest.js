c620b61712d39967bbbde47eb8bacbf29650e2fe
unknown <G513545@ISDEVL-G513545.genmills.com>
2013-04-06 16:54:05 -0500

    added makeBox function to castleTest

--- scripts/castleTest.js
+++ scripts/castleTest.js
@@ -1,6 +1,40 @@
 define(["world"], function (world) {
 	
-	  return { addShape: function()
+	  return { 
+	  makeBox: function(x,y,width,height)
+	  {
+	  var   b2Vec2 = Box2D.Common.Math.b2Vec2
+         	,	b2BodyDef = Box2D.Dynamics.b2BodyDef
+         	,	b2Body = Box2D.Dynamics.b2Body
+         	,	b2FixtureDef = Box2D.Dynamics.b2FixtureDef
+         	,	b2Fixture = Box2D.Dynamics.b2Fixture
+         	,	b2World = Box2D.Dynamics.b2World
+         	,	b2MassData = Box2D.Collision.Shapes.b2MassData
+         	,	b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape
+         	,	b2CircleShape = Box2D.Collision.Shapes.b2CircleShape
+         	,	b2DebugDraw = Box2D.Dynamics.b2DebugDraw
+            ;
+	  
+	    var fixDef = new b2FixtureDef;
+         fixDef.density = 1.0;
+         fixDef.friction = 0.5;
+         fixDef.restitution = 0.2;
+	  
+	    var bodyDef = new b2BodyDef;
+		bodyDef.type = b2Body.b2_dynamicBody;
+	 
+		
+		   fixDef.shape = new b2PolygonShape;
+		   fixDef.shape.SetAsBox(
+				 width //half width
+			  ,  height //half height
+		   );
+		 
+		bodyDef.position.x = x;
+		bodyDef.position.y = y;
+		world.CreateBody(bodyDef).CreateFixture(fixDef);
+	  },
+	  addShape: function()
 	  {
 	     var   b2Vec2 = Box2D.Common.Math.b2Vec2
          	,	b2BodyDef = Box2D.Dynamics.b2BodyDef
